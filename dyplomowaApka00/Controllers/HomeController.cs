29c2bc19308eb7f378e987e3d8e25e20a6af857e
elektromntr <bartosztadeuszwojcik@gmail.com>
2018-04-10 21:25:55 +0200

    Email form work in progress

--- dyplomowaApka00/Controllers/HomeController.cs
+++ dyplomowaApka00/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dyplomowaApka00.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace dyplomowaApka00.Controllers
 {
@@ -25,6 +29,39 @@ namespace dyplomowaApka00.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        //email form
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("bartosz@ablab.pl"));  // replace with valid value 
+                message.From = new MailAddress("sogoSender@outlook.com");  // replace with valid value
+                message.Subject = "Temat wiadomosci";
+                message.Body = string.Format(body, model.FromName, model.FromPhone, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "sogoSender@outlook.com",  // replace with valid value
+                        Password = "S0g053nder"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ dyplomowaApka00/Models/EmailFormModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace dyplomowaApka00.Models
+{
+    public class EmailFormModel
+    {
+        [Display(Name = "Twoje imię")]
+        public string FromName { get; set; }
+
+        [Display(Name = "Twój email")]
+        public string FromEmail { get; set; }
+
+        [Required, Display(Name = "Twój numer telefonu"), Phone]
+        [RegularExpression(@"\d{9}", ErrorMessage = "Podaj 9-cyfrowy numer telefonu komórkowego")]
+        public string FromPhone { get; set; }
+
+        public string Message { get; set; }
+    }
+}
